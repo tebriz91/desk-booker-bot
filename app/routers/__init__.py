87027a038170331487c250fbd1dc1affc472ef8c
tebriz91 <tebriz.tagiyev@gmail.com>
2024-03-23 14:28:51 +0300

    Refactor admin router imports

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,9 +1,9 @@
 from aiogram import Router
 
-from routers.admin import admin_router
-from routers.user import user_router
-# from routers.error import error_router
-# from routers.other import other_router
+from .admin import admin_router
+from .user import user_router
+# from .error import error_router
+# from .other import other_router
 
 router = Router(name='main')
 router.include_routers(
--- app/routers/admin/__init__.py
+++ app/routers/admin/__init__.py
@@ -1,8 +1,3 @@
-from routers.admin.admin import *
-from routers.admin.desk_management import *
-from routers.admin.room_management import *
-from routers.admin.user_management import *
-
-from routers.admin.router import admin_router
+from .router import admin_router
 
 __all__ = ("admin_router",)
