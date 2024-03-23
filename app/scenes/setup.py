62b9cc6c1b0b504c559c66b7cfa9677a3f945c2e
tebriz91 <tebriz.tagiyev@gmail.com>
2024-03-23 21:18:53 +0300

    Add user book date select scene

--- app/scenes/setup.py
+++ app/scenes/setup.py
@@ -3,16 +3,20 @@ from aiogram.fsm.scene import SceneRegistry
 from aiogram.filters import Command
 
 from routers.admin.router import admin_router
+from routers.user.router import user_router
 
 from scenes.admin.main import AdminMenuScene
+from scenes.user.book.date_select import DateSelectScene
 
 def register_scenes(dispatcher: Dispatcher):
     # Create a SceneRegistry object
     scene_registry = SceneRegistry(dispatcher)
     
-    # Dynamically import and register all admin scenes
+    # Dynamically import and register all admin and user scenes
     from scenes.admin import __all__ as admin_scenes
     scene_registry.register(*admin_scenes)
+    from scenes.user import __all__ as user_scenes
+    scene_registry.register(*user_scenes)
 
     # If you have scenes outside of the admin subpackage, import and register them similarly
     # from scenes.user import __all__ as user_scenes
@@ -20,3 +24,4 @@ def register_scenes(dispatcher: Dispatcher):
     
     # Register entry-point handlers for the scenes
     admin_router.message.register(AdminMenuScene.as_handler(), Command("admin"))
+    user_router.message.register(DateSelectScene.as_handler(), Command("book"))
---
+++ app/scenes/user/__init__.py
@@ -0,0 +1,15 @@
+from .book.date_select import DateSelectScene
+
+# TODO: Add SelectRoomScene, SelectDeskScene
+
+# from .cancel_booking import CancelBookingScene
+
+# from .my_bookings import MyBookingsScene
+
+# from .all_bookings import AllBookingsScene
+
+# from .help import HelpScene
+
+__all__ = [
+    DateSelectScene,
+    ]
