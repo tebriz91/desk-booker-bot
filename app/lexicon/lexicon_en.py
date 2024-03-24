be8afaf4665bfe9268ccd7bfca6d761cc20b7488
tebriz91 <tebriz.tagiyev@gmail.com>
2024-03-24 23:17:30 +0300

    Refactor menu commands to use an Enum

--- app/lexicon/lexicon_en.py
+++
@@ -1,8 +0,0 @@
-LEXICON_COMMANDS_EN: dict[str, str] = {
-    '/book': 'Book a Desk',
-    '/cancel_bookings': 'Cancel Booking',
-    '/my_bookings': 'View My Bookings',
-    '/all_bookings': 'View All Bookings',
-    '/help': 'Help Information',
-    '/admin': 'Admin Commands',
-}
