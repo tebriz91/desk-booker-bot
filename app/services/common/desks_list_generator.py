2e2e98d6a0b6f5d369570b982466973e471277ea
tebriz91 <tebriz.tagiyev@gmail.com>
2024-04-06 12:44:14 +0300

    Refactored desk_list_generator module

--- app/services/common/desks_list_generator.py
+++ app/services/common/desks_list_generator.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 from datetime import datetime
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,10 +8,10 @@ from database.orm_queries import (
     orm_select_room_id_by_name,
     orm_select_available_not_assigned_desks_by_room_id,
     orm_select_user_team_id,
-    orm_select_team_preffered_room_id,
+    orm_select_team_preferred_room_id,
     orm_select_available_not_booked_desks_by_room_id,
 )
-from database.enums.weekdays import Weekday
+from database.enums.weekdays import Weekday # TODO: Implement Weekday enum
 
 from utils.logger import Logger
 
@@ -22,7 +22,30 @@ async def generate_available_desks_list(session: AsyncSession, room_name: str) -
     desks_orm_obj = await orm_select_available_desks_by_room_name(session, room_name)
     return [desk.name for desk in desks_orm_obj]
 
-async def generate_available_not_booked_desks_list(
+
+async def parse_booking_date(date: str, date_format: str) -> Union[datetime.date, str]:
+    try:
+        return datetime.strptime(date, date_format).date()
+    except ValueError:
+        return "Error: Incorrect date format. Parsing date to datetime.date failed."
+
+
+async def fetch_desk_lists(
+    session: AsyncSession,
+    room_id: int,
+    weekday: int,
+    booking_date: datetime.date
+    ) -> Tuple[List[str], List[str]]:
+    try:
+        not_assigned_desks = await orm_select_available_not_assigned_desks_by_room_id(session, room_id, weekday)
+        not_booked_desks_obj = await orm_select_available_not_booked_desks_by_room_id(session, room_id, booking_date)
+        not_booked_desks = [desk.name for desk in not_booked_desks_obj]
+        return not_assigned_desks, not_booked_desks
+    except Exception as e:
+        raise RuntimeError(f"Error retrieving desks: {e}")
+
+
+async def generate_desks_list(
     session: AsyncSession,
     room_name: str,
     date: str,
@@ -31,81 +54,50 @@ async def generate_available_not_booked_desks_list(
     telegram_id: Optional[int],
     standard_access_days: Optional[int] = 1,
     ) -> Union[List[str], str]:
-    desks: List[str] = []
     
-    # Convert date string to date object
-    try:
-        booking_date = datetime.strptime(date, date_format).date()
-    except ValueError:
-        return f"Error: Incorrect date format. Parsing date to datetime.date failed."
+    booking_date = await parse_booking_date(date, date_format)
+    if isinstance(booking_date, str):
+        return booking_date  # Return error message if parsing failed
     
     today = datetime.now().date()
     logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Today: {today}")
-    weekday = booking_date.weekday()  # This will be an integer from 0 to 6
-    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Weekday: {weekday}")
     days_difference = (booking_date - today).days
     logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Days difference: {days_difference}")
+    if days_difference < 0:
+        return f"Error: Selected date: {booking_date} is in the past"
+    
     # Fetch the room ID based on room name
     room_id = await orm_select_room_id_by_name(session, room_name)
     logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Room ID: {room_id}")
-    
-    # Retrieve desks from the selected room, which are not assigned to any user on the selected weekday
+    if not room_id:
+        return f"Error: Room '{room_name}' not found"
+
+    # Advanced mode logic
     try:
-        not_assigned_desks = await orm_select_available_not_assigned_desks_by_room_id(session, room_id, weekday)
-        logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Not assigned desks: {not_assigned_desks}")
-    except Exception as e:
-        return f"Error retrieving not assigned desks: {e}"
-    
-    # If advanced mode is enabled, retrieve user's team_id, than check if this team has a preffered room, than check if selected room is the same as preffered room. If yes, return desks from this room. If not, check if standard access days equals or less than number of days between today and booking date. If yes, return desks from selected room. If not, return empty list.
-    
-    try:
-        not_booked_desks_obj = await orm_select_available_not_booked_desks_by_room_id(session, room_id, booking_date)
-        not_booked_desks = [desk.name for desk in not_booked_desks_obj]
-        logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Not booked desks: {not_booked_desks}")
-    except Exception as e:
-        return f"Error retrieving not booked desks: {e}"
-    
-    try:
-        
-        if not advanced_mode:
-            desks = list(set(not_assigned_desks) & set(not_booked_desks))
-            logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Desks (in basic mode - service level): {desks}")
-            return desks
-        
-        if advanced_mode and telegram_id:
-            # Fetch user's team ID
+        if advanced_mode:
             team_id = await orm_select_user_team_id(session, telegram_id)
-            logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Advanced mode. Team ID: {team_id}")
-            
-            if not team_id and days_difference > standard_access_days:
-                logger.info(f">>>>>>>>>>>>>>>>>>>>>>>No team ID and days difference > standard access days, desks: {desks}")
-                return desks
-            
-            if not team_id and days_difference <= standard_access_days:
+            preferred_room_id = await orm_select_team_preferred_room_id(session, team_id) if team_id else None
+            # User can book if (one of the following conditions is met):
+            # - No team_id or preferred_room_id != room_id but within standard access days
+            # - Preferred room matches selected room despite days difference
+            if preferred_room_id != room_id and days_difference > standard_access_days:
+                return []
+            elif not team_id and days_difference > standard_access_days:
+                return []
+            elif not team_id or preferred_room_id == room_id or days_difference <= standard_access_days:
+                not_assigned_desks, not_booked_desks = await fetch_desk_lists(session, room_id, booking_date.weekday(), booking_date)
                 desks = list(set(not_assigned_desks) & set(not_booked_desks))
-                logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Desks (not team_id and days_difference <= standard_access_days): {desks}")
-                return desks
-            
-            if team_id:
-                # Fetch team's preffered room
-                preffered_room_id = await orm_select_team_preffered_room_id(session, team_id)
-                logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Preffered room ID: {preffered_room_id}")
-                
-                if preffered_room_id and preffered_room_id != room_id and days_difference > standard_access_days:
-                    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Preffered room ID != room ID and days difference > standard access days")
-                    return desks
-                
-                if preffered_room_id and preffered_room_id != room_id and days_difference <= standard_access_days:
-                    desks = list(set(not_assigned_desks) & set(not_booked_desks))
-                    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Desks (if preffered_room_id and preffered_room_id != room_id and days_difference <= standard_access_days): {desks}")
-                    return desks
-                
-                if preffered_room_id and preffered_room_id == room_id:
-                    desks = list(set(not_assigned_desks) & set(not_booked_desks))
-                    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>Desks (if preffered_room_id and preffered_room_id == room_id): {desks}")
-                    return desks
-    
+                return desks if desks else []
+            else:
+                raise RuntimeError("Error: Unknown advanced mode logic")
     except Exception as e:
-        return f"Error: {e}"
+        raise RuntimeError(f"Error in advanced mode logic: {e}")
     
-    return desks
+    # Non-advanced mode logic
+    try:
+        if not advanced_mode:
+            not_assigned_desks, not_booked_desks = await fetch_desk_lists(session, room_id, booking_date.weekday(), booking_date)
+            desks = list(set(not_assigned_desks) & set(not_booked_desks))
+            return desks if desks else []
+    except Exception as e:
+        raise RuntimeError(f"Error in non-advanced mode logic: {e}")
