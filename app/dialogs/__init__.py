1c921c7919db818c337e4832ed3c6c6e7f4c4df4
tebriz91 <tebriz.tagiyev@gmail.com>
2024-04-13 20:21:36 +0300

    Refactor dialog initialization and registration in __init__.py

--- app/dialogs/__init__.py
+++ app/dialogs/__init__.py
@@ -1,7 +1,16 @@
-'''from aiogram import Router
+from aiogram import Router
+from dialogs.booking.dialogs import booking_dialog
+from dialogs.cancel_bookings.dialogs import cancel_bookings_dialog
+from dialogs.all_bookings.dialogs import all_bookings_dialog
+from dialogs.desk.dialogs import desk_dialog
 
-from dialogs.booking import bot_menu_dialogs
-
-def setup_all_dialogs(router: Router):
-    for dialog in bot_menu_dialogs():
-        router.include_router(dialog)'''
+def register_dialogs(router: Router):
+    # List of all dialog modules
+    dialogs = [
+        booking_dialog,
+        cancel_bookings_dialog,
+        all_bookings_dialog,
+        desk_dialog,
+    ]
+    # Use include_routers to add all dialogs at once
+    router.include_routers(*dialogs)
