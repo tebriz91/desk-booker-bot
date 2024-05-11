09c285f0cd89c10e93e503a8c0277f7970566456
tebriz91 <tebriz.tagiyev@gmail.com>
2024-05-11 19:15:12 +0300

    Add utils.py with test data and util functions for integration test

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -1,5 +1,7 @@
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.database.models import Base
 from app.utils.logger import Logger
 logger = Logger()
 
@@ -27,27 +29,74 @@ TEST_ROOMS: list[str] = [
 
 
 TEST_DESKS: dict[str, list[str]] = {
-    "Gryffindor Room": ["Desk 1", "Desk 2", "Desk 3", "Desk 4"],
-    "Slytherin Room": ["Desk 1", "Desk 2", "Desk 3", "Desk 4"],
-    "Ravenclaw Room": ["Desk 1", "Desk 2", "Desk 3", "Desk 4"],
-    "Hufflepuff Room": ["Desk 1", "Desk 2", "Desk 3", "Desk 4"],
-    "Room of Requirement": ["Desk 1", "Desk 2", "Desk 3", "Desk 4"],
-    "Great Hall": ["Desk 1", "Desk 2", "Desk 3", "Desk 4"],
+    "Gryffindor Room": [
+        "Desk 1",
+        "Desk 2",
+        "Desk 3",
+        "Desk 4",
+        "Desk 5",
+    ],
+    "Slytherin Room": [
+        "Desk 6",
+        "Desk 7",
+        "Desk 8",
+        "Desk 9",
+        "Desk 10",
+    ],
+    "Ravenclaw Room": [
+        "Desk 11",
+        "Desk 12",
+        "Desk 13",
+        "Desk 14",
+        "Desk 15",
+    ],
+    "Hufflepuff Room": [
+        "Desk 16",
+        "Desk 17",
+        "Desk 18",
+        "Desk 19",
+        "Desk 20",
+    ],
+    "Room of Requirement": [
+        "Desk 21",
+        "Desk 22",
+        "Desk 23",
+        "Desk 24",
+        "Desk 25",
+    ],
+    "Great Hall": [
+        "Desk 26",
+        "Desk 27",
+        "Desk 28",
+        "Desk 29",
+        "Desk 30",
+    ],
 }
 
 
 async def safe_insert(session: AsyncSession, insert_function, data, entity_name: str) -> bool:
     """
     Generic insert function to handle adding entities to the database.
-    Logs the operation and commits or rolls back the transaction based on success.
+    This version does not commit or rollback, as these actions will be managed externally.
     """
     try:
-        logger.info(f"Adding test {entity_name} to the database...")
         await insert_function(session, data)
-        await session.commit()
         logger.info(f"{entity_name.capitalize()} have been added to the database.")
         return True
     except Exception as e:
-        await session.rollback()
         logger.error(f"Failed to insert {entity_name}: {e}")
         return False
+
+
+async def create_db(engine):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def truncate_db_cascade(session: AsyncSession):
+    if session.in_transaction():
+        await session.rollback()  # Ensure no transactions are open
+    async with session.begin():
+        await session.execute(text("SET CONSTRAINTS ALL DEFERRED;"))  # Optional
+        for table_name in reversed(Base.metadata.sorted_tables):
+            await session.execute(text(f"TRUNCATE TABLE {table_name.name} CASCADE;"))
