a5d561a5437b8aca63c34759e0ed4f8db9539236
tebriz91 <tebriz.tagiyev@gmail.com>
2024-05-25 16:40:17 +0300

    refactor: Update readability and type notation of database engine and session functions

--- app/database/engine.py
+++ app/database/engine.py
@@ -1,52 +1,38 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy import text
 
 from app.database.models import Base
 
 
-engine = None  # Declare engine as a global variable to be initialized later
-session_maker = None  # Same for session_maker
+def get_engine(db_url: str, echo: bool = False) -> AsyncEngine:
+    """Initialize async engine."""
+    engine: AsyncEngine = create_async_engine(db_url, echo=echo)
+    return engine
 
 
-def initialize_engine(db_url, echo=True):
-    """
-    Initialize the async engine and session maker using the database URL.
-    This function should be called with configuration data from outside.
-    """
-    global engine, session_maker
-    engine = create_async_engine(db_url, echo=echo)
-    session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-
-
-def get_session_maker():
-    """
-    Returns the session maker. Ensures that the session maker is initialized.
-    This function can be called to obtain the session maker without directly
-    accessing the global variable, providing a layer of abstraction.
-    """
-    if session_maker is None:
-        raise Exception("Database engine and session maker have not been initialized. Call initialize_engine first.")
+def get_session_maker(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+    """Initialize session maker."""
+    session_maker: async_sessionmaker[AsyncSession] = async_sessionmaker(
+        bind=engine,
+        expire_on_commit=False,
+        class_=AsyncSession)
     return session_maker
 
 
-async def create_db():
-    """
-    Create database tables based on models.
-    Requires the engine to be initialized first.
-    """
+async def create_db(engine: AsyncEngine) -> None:
+    """Create database tables based on models."""
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
     
-async def drop_db():
-    """
-    Drop all database tables.
-    """
+async def drop_db(engine: AsyncEngine) -> None:
+    """Drop all database tables."""
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
 
-async def drop_db_cascade():
+async def drop_db_cascade(engine: AsyncEngine) -> None:
+    """Drop all database tables with CASCADE and enums."""
     async with engine.begin() as conn:
         # Reflecting all tables from the database
         await conn.run_sync(Base.metadata.reflect)
