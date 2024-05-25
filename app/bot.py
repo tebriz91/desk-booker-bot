7702bc88ba9c696a17a27a312582a92a159e55cb
tebriz91 <tebriz.tagiyev@gmail.com>
2024-05-25 16:41:39 +0300

    refactor: Update readability and type notation of main and setup functions

--- app/bot.py
+++ app/bot.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import sys
 from pathlib import Path
 project_root = Path(__file__).parent.parent
@@ -12,6 +13,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage, Redis, DefaultKeyBuilder
 from aiogram.fsm.storage.memory import SimpleEventIsolation
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from fluentogram import TranslatorHub # type: ignore
 from app.middlewares.i18n import TranslatorRunnerMiddleware
@@ -20,33 +22,25 @@ from app.utils.i18n import Translator
 from aiogram_dialog import setup_dialogs
 from app.dialogs import register_dialogs
 
-from app.config_data.config import load_config
+from app.config_data.config import Config, load_config
 from app.utils.bot_description import set_bot_description
 from app.keyboards.set_menu import set_main_menu
 from app.scenes.setup import register_scenes
 from app.middlewares.user_middleware import UserMiddleware
 # from app.middlewares.config_middleware import ConfigMiddleware
 from app.middlewares.db_middleware import DataBaseSession
-from app.database.engine import (
-    initialize_engine,
-    get_session_maker,
-    create_db,
-    drop_db_cascade, #! For development purposes only
-    )
-from app.utils.logger import Logger
+from app.database.engine import get_engine, get_session_maker
 from app.routers import router
 
 
-# Configuration Loading
-config = load_config()
-
-
-# Logger Setup
+from app.utils.logger import Logger
 logger = Logger()
 
 
-# Bot Initialization
-def initialize_bot():
+config: Config = load_config()
+
+
+def setup_bot() -> Tuple[Bot, RedisStorage]:
     """
     Initializes the bot with token and properties defined in the config.
     Sets up Redis storage for managing state and sessions.
@@ -57,20 +51,20 @@ def initialize_bot():
     bot = Bot(
         token=config.bot.token,
         default=DefaultBotProperties(
-            parse_mode=ParseMode.HTML, # HTML as default parse mode for messages
-            link_preview_is_disabled=False, # Enable link previews
+            parse_mode=ParseMode.HTML,
+            link_preview_is_disabled=False,
             link_preview_prefer_large_media=True,
             link_preview_show_above_text=False)) # Show link previews below text
     return bot, storage
 
 
-# Dispatcher Setup
-def setup_dispatcher(bot, storage):
+def setup_dispatcher(bot, storage) -> Dispatcher:
     """
-    Configures the Dispatcher with the bot instance and storage mechanism.
-    Includes routers and registers scenes for handling different bot commands and interactions.
+    Configures the Dispatcher with the bot instance and storage.
+    Includes routers and registers scenes.
     """    
     dp = Dispatcher(
+        bot=bot,
         storage=storage,
         events_isolation=SimpleEventIsolation(),
     )
@@ -82,17 +76,24 @@ def setup_dispatcher(bot, storage):
     return dp
 
 
-# Middleware Setup
-def setup_middlewares(dp):
-    """
-    Applies middlewares to the Dispatcher for pre-processing messages and updates. This includes setting up session management and user data handling.
-    """
-    session_maker = get_session_maker()
-    dp.message.outer_middleware(UserMiddleware(session_pool=session_maker)) # This middleware checks if the user is registered in the database
-    dp.callback_query.outer_middleware(UserMiddleware(session_pool=session_maker))
-    dp.update.middleware(DataBaseSession(session_pool=session_maker)) # This middleware provides a database session to the handler
-    dp.update.middleware(TranslatorRunnerMiddleware(translator=Translator())) # This middleware provides translation services to the handler
-    # dp.update.middleware(ConfigMiddleware(config=config))
+def setup_database() -> async_sessionmaker[AsyncSession]:
+    engine = get_engine(db_url=config.db.url, echo=False)
+    session_maker = get_session_maker(engine)
+    return session_maker
+
+
+def setup_middlewares(dp, session_maker) -> None:
+    """Applies middlewares to the Dispatcher for pre-processing messages and updates."""
+    dp.message.outer_middleware(
+        UserMiddleware(session_pool=session_maker))
+    dp.callback_query.outer_middleware(
+        UserMiddleware(session_pool=session_maker))
+    dp.update.middleware(
+        DataBaseSession(session_pool=session_maker))
+    dp.update.middleware(
+        TranslatorRunnerMiddleware(translator=Translator()))
+    # dp.update.middleware(
+        # ConfigMiddleware(config=config))
 
 
 async def on_startup(bot):
@@ -108,41 +109,17 @@ async def on_shutdown(bot):
 
 
 async def main():
-    """
-    The main coroutine setups the bot, the dispatcher, and the middlewares.
-    It handles startup events like database initialization, bot registration,
-    middleware setup, and starts polling for updates.
-    
-    The flow is as follows:
-    1. Initialize the database engine with the configuration from the config file.
-    2. Create the database schema if it doesn't exist.
-    3. Initialize the bot with its token and default properties.
-    4. Setup the dispatcher with the bot instance and session storage.
-    5. Apply middlewares for user session management and database access.
-    6. Register the startup and shutdown events for logging and resource management.
-    7. Delete any existing webhook configurations to ensure clean startup.
-    8. Setup the bot's main menu for user interaction.
-    9. Start polling for updates to begin processing incoming messages and commands.
-    """
-    # Initialize the database engine with configuration parameters.
-    initialize_engine(config.db.url, echo=False)
-    
-    #! Drop the database schema if it exists. This is for development purposes only.
-    # await drop_db_cascade()
-    
-    # Create the database schema based on defined models.
-    # It doesn't affect existing tables that match the schema.
-    await create_db()
-    
-    bot, storage = initialize_bot()
+    """The main coroutine setups: bot, dispatcher, database, middlewares and starts polling for updates."""
+    bot, storage = setup_bot()
     dp = setup_dispatcher(bot, storage)
-    translator_hub: TranslatorHub = Translator() # type: ignore
-    setup_middlewares(dp)
+    session_maker = setup_database()
+    setup_middlewares(dp, session_maker)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
-    await bot.delete_webhook(drop_pending_updates=True) # Remove any existing webhooks before starting to poll for updates
+    await bot.delete_webhook(drop_pending_updates=True) # Removes any existing webhooks before starting polling for updates
     await set_bot_description(bot)
     await set_main_menu(bot) # Setup the bot's main menu
+    translator_hub: TranslatorHub = Translator() # type: ignore
     await dp.start_polling(
         bot,
         _translator_hub=translator_hub,
