4f07d60f07a487d56d3cfeae52da7c21a2568ae3
tebriz91 <tebriz.tagiyev@gmail.com>
2024-05-26 19:42:21 +0300

    update host and port

--- api/main.py
+++ api/main.py
@@ -87,8 +87,8 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "main:app",
-        host=config.sqladmin.host,
-        port=config.sqladmin.port,
+        host="0.0.0.0",
+        port=8000,
         reload=True,
         log_level="info",
         lifespan="on",
