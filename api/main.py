26c5764971e211f307a6b4ea7efb9b2ee022997f
tebriz91 <tebriz.tagiyev@gmail.com>
2024-06-04 22:20:36 +0300

    refactor: Update uvicorn run configuration to use host and port from config file

--- api/main.py
+++ api/main.py
@@ -87,8 +87,8 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
-        port=8000,
+        host=config.sqladmin.host,
+        port=config.sqladmin.port,
         reload=True,
         log_level="info",
         lifespan="on",
